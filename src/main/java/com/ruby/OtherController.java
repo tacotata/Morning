e7199d80ba5783f9bbe5538bf3c9ff0dc933cead
tacotata <71251163+tacotata@users.noreply.github.com>
2022-03-09 21:00:43 +0900

    롬복 추가

--- src/main/java/com/ruby/OtherController.java
+++ src/main/java/com/ruby/OtherController.java
@@ -2,10 +2,13 @@ package com.ruby;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 //패키지가 다른 otherController 포함시키려면 @ComponentScan 어노테이션 추가해서 스캔하고자 하는 패키지를 직접지정
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.rubypaper", "com.ruby"})
+//0309  componentScan 안됨 확인예정
 public class OtherController {
 	public OtherController() {
 		System.out.println("===>OtherController 생성");
--- src/main/java/com/rubypaper/domain/BoardVO.java
+++ src/main/java/com/rubypaper/domain/BoardVO.java
@@ -2,6 +2,10 @@ package com.rubypaper.domain;
 
 import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /*@RestController는 JSP같은 뷰를 별도로 만들지 ㅇ낳는 대신에 컨트롤러 메소드가 리턴한 데이터 자체를 클라이언트로 전달한다 
  * 클라이언트에 전달되는 데이터는 대부분 문자열이거나 VO(value object)나 컬렉션 형태의 자바 객체인데,
  * 자바 객체가 전달되는 경우에는 자동으로 JSON으로 변환하여 처리하게 된다
@@ -15,55 +19,30 @@ import java.util.Date;
  *   이런 경우도 @RextController는  JSON으로 변환하여 처리한ㄴ다 
  * */
 
-//git
+
 ///6개의 멤버변수  
 //Getter/setter 메소드는  alt +sihft + s 
+@Getter
+@Setter
+@ToString
 public class BoardVO {
 	private int seq;
 	private String title; 
 	private String writer;
 	private String content;
 	private Date CreateDate = new Date();
-	private int cnt = 0;
-	public int getSeq() {
-		return seq;
-	}
-	public void setSeq(int seq) {
-		this.seq = seq;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Date getCreateDate() {
-		return CreateDate;
-	}
-	public void setCreateDate(Date createDate) {
-		CreateDate = createDate;
-	}
-	public int getCnt() {
-		return cnt;
-	}
-	public void setCnt(int cnt) {
-		this.cnt = cnt;
-	} 
-
+	private int cnt = 0;	
 
 	
 }
+/*롭복 추가 
+ *  Getter Setter 메소드 만들어줌 
+ *  @RequiredArgsConstructor 모든 멤버변수를 초기화하는 생성자 만들어준다 
+ *  @ToString 모든 멤버 변수의 값을 문자열로 연결하여 리턴하는 toString()메소드를 만들어준다
+ *  @EqualsAndHashCode equls(),hashCode()메소드를 만들어준다
+ *  @Date-> @Getter ,@Setter, @ToString, @RequiredArgsConstructor, @EqualsAndHashCode 모두 포함한다
 
+
+ * 
+ * */
 
